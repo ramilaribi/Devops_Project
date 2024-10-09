b291c03337a00d4b6d3d2a4e4be35678b3c8ac5c
ramilaribi <laribi.rami@esprit.tn>
2024-10-09 20:13:49 +0100

    enabling cors security for ip of vm

--- src/main/java/com/example/backend_voltix/config/WebConfig.java
+++ src/main/java/com/example/backend_voltix/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://192.168.1.109:8089", "http://localhost:8089")
+                .allowedOrigins("http://192.168.1.109:8089","http://192.168.33.10:8086", "http://localhost:8089")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
