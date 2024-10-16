cb51b0cc9f0e70000d56188c57e732f4037d93ec
ramilaribi <laribi.rami@esprit.tn>
2024-10-16 20:28:52 +0100

    testing New Release 🔧

--- src/test/java/com/example/backend_voltix/BackendVoltixApplicationTests.java
+++ src/test/java/com/example/backend_voltix/BackendVoltixApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.backend_voltix;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BackendVoltixApplicationTests {
 
     @Test
